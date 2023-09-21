a02227a7c0ba428bf5c33203bba0bfa2d18c4c1f
Antonelia Valenti <antonelia.valenti@hotmail.com>
2023-09-21 15:35:18 -0300

    segunda preentrega

--- index2.js
+++ index2.js
@@ -1,56 +1,119 @@
-const nameInput = prompt("BIENVENIDO! Dinos tu nombre: ");
+let person1 = newUser();
+console.log(person1);
 
-const age = prompt("Hola " + nameInput + " por favor, dinos tu edad: ");
-if (age > 1 && age < 18) {
-    alert("Disculpa, " + nameInput + " no puedes frecuentar este sitio! Consulta con tu nutricionista de confianza!!");
-} else {
-    let preferSelect;
-    let precioLibroRecetas;
-    let precioRutinaEjercicio;
-    let costoTotal; 
+let person2 = newUser();
+console.log(person2);
+ 
+let person3 = {
+    nameImput : "Soledad",
+    ageInput : 29,
+    diet : "vegana",
+    rutine:"no"
+}
+console.log(person3);
 
+let person4 = newUser();
+console.log(person4);
+
+let person5 = {
+    nameInput : "Mara",
+    ageInput : 27,
+    diet : "vegana",
+    rutine: "si",
+}
+console.log(person5);
+
+
+let usuer = [person1, person2, person3, person4, person5]
+console.log(usuer);
+
+
+const findDiet = usuer.filter((el) => el.diet == "vegana");
+console.log(findDiet);
+
+
+function newUser (){
+    let persona = {
+        nameInput : "",
+        ageInput : "",
+        diet : "",
+        rutine: "",
+        };
+        persona.nameInput = validarNombre(prompt("BIENVENIDO! Dinos tu nombre: "));
+        persona.ageInput = prompt("Hola " + persona.nameInput + " por favor, dinos tu edad: ");
+
+        if 
+        (checkAge(persona.ageInput)) {
+    
+            persona.diet = CheckDiet(); 
+            persona.rutine = checkRutine();
+            alert("Muchas gracias por utilizar nuestro Servicio! El plan de salud será enviado por mail dentro de las proximas 48 horas")         
+        
+        } else {
+            alert("Por favor, ingrese una edad válida. Si sos menor a 18 años, debes consultar con tu nutricionista de confianza!")
+            persona.ageInput = "Menor";
+            persona.diet = "No aplica";
+            persona.rutine = "No aplica";
+        }
+        return persona
+}
+
+
+function checkAge(age) { 
+    if  (isNaN(age) || age <= 18 || !age) {
+      return false;
+    }  
+    else  return true;
+}
+
+
+function CheckDiet (preferSelect) {
   
     while (true) {
-        preferSelect = prompt("PERFECTO! Sigamos... Pasemos a tus gustos preferidos: 1-Variado, 2-Vegetariano, 3-Vegano, 4-Proteico, 5-Fit");
+     preferSelect = prompt("PERFECTO! Sigamos... Pasemos a tus gustos preferidos: 1-Variada, 2-Vegetariana, 3-Vegana, 4-Proteica, 5-Fit");
 
         switch (preferSelect) {
-            case "1":
-                precioLibroRecetas = 10;
+            case "variada":
+                select = "Variada";
                 break;
-            case "2":
-                precioLibroRecetas = 20;
+            case "vegetariana":
+                select = "Vegetariana";
                 break;
-            case "3":
-                precioLibroRecetas = 30;
+            case "vegana":
+                select = "Vegana";
                 break;
-            case "4":
-                precioLibroRecetas = 40;
+            case "proteica":
+                select = "Proteíca";
                 break;
-            case "5":
-                precioLibroRecetas = 50;
+            case "fit":
+                select = "Fit";
                 break;
             default:
                 alert("Opción no válida, elige una opción válida.");
                 continue;
         }
+        alert("Tu eleccion de recetario fue " + select)
+        return preferSelect
+        
 
-    
-        const rutinaEjercicio = prompt("¿Deseas una rutina de ejercitación? (Sí/No)").toLowerCase();
-        if (rutinaEjercicio === "sí" || rutinaEjercicio === "si") {
-            precioRutinaEjercicio = 100; 
-        } else {
-            precioRutinaEjercicio = 0; 
-        }
- 
-      
-        calcularCostoTotal(precioLibroRecetas, precioRutinaEjercicio)
-        break;
     }   
-}  
-function calcularCostoTotal (precioLibroRecetas, precioRutinaEjercicio) {
-        let resultado = precioLibroRecetas + precioRutinaEjercicio;
-        alert("El costo total es: $" + resultado);
-        return resultado;
 }
+function checkRutine (precioRutinaEjercicio) {
+ let rutinaEjercicio = prompt("¿Deseas una rutina de ejercitación? (Sí/No)").toLowerCase();
+        if (rutinaEjercicio === "sí" || rutinaEjercicio === "si") {
+            precioRutinaEjercicio = "Requerida"; 
+            alert("La rutina fue " + precioRutinaEjercicio)
+        } else {
+            precioRutinaEjercicio = "No requerida"; 
+            alert("Con el código: RUTINE20, obtenes un 20% de descuento si solicitas tu rutina dentro de los próximos 30 días!!")
+        }
+        return precioRutinaEjercicio
+    } 
 
-//terminado
+function validarNombre(nombre) {
+    while (!nombre || !isNaN(nombre)) {
+      alert('Por favor, ingrese un nombre válido (no vacío y sin números).');
+      nombre = prompt("BIENVENIDO! Dinos tu nombre: ");
+    }
+        return nombre;
+}

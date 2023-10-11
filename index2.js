a9ee2de39b015e17747238fd0980d8cc6db92dfe
Antonelia Valenti <antonelia.valenti@hotmail.com>
2023-10-11 17:50:21 -0300

    cambios

--- index2.js
+++ index2.js
@@ -19,7 +19,6 @@ function totalUser() {
         mail: datos_mail.value
     };
     user_list.push(newUser);
-    return newUser;
 }
 
 button.addEventListener('click', () => {
@@ -27,62 +26,60 @@ button.addEventListener('click', () => {
         alert("Por favor, complete todos los campos.");
         return;
     }
-    const newUser = totalUser();
-    localStorage.setItem("usuarios", JSON.stringify(user_list));
-    
-    let usuarioHTML = `
-        <div>
-            <h2>${newUser.nombre}</h2>
-            <p>${newUser.age}</p>
-        </div>
-        <p>${newUser.diet}</p>
-        <p>${newUser.rutine}</p>
-        <p>${newUser.mail}</p>
-        <p id="parrafoStatus-${newUser.nombre}" class="${newUser.status.toLowerCase()}">${newUser.status}</p>
-        <div>
-            <button id="botonDato-${newUser.nombre}" class="boton_dato" type="button">Cambiar Estado</button>
-        </div>
-    `;
-    
 
+    totalUser();
+    localStorage.setItem("usuarios", JSON.stringify(user_list));
+  
+    renderizarUsuario(user_list);
+    
     datos_username.value = "";
     datos_age.value = "";
     datos_diet.value = "";
     datos_rutine.value = "";
     datos_status.value = "";
     datos_mail.value = "";
-
-   
 });
 
-user_list.forEach(totalUser){
-    let usuario = document.createElement('article');
-    usuario.className = "datos";
-    usuario.innerHTML = usuarioHTML;
-    datos_container.appendChild(usuario);
+function renderizarUsuario(user_list) {
+    datos_container.innerHTML = ""
+    user_list.forEach((usuario) => {
+    let usuarioHTML = `
+        <div>
+            <h2>${usuario.nombre}</h2>
+            <p>${usuario.age}</p>
+        </div>
+        <p>${usuario.diet}</p>
+        <p>${usuario.rutine}</p>
+        <p>${usuario.mail}</p>
+        <p id="parrafoStatus-${usuario.nombre}" class="${usuario.status.toLowerCase()}">${usuario.status}</p>
+        <div>
+            <button id="botonDato-${usuario.nombre}" class="boton_dato" type="button">Cambiar Estado</button>
+        </div>
+    `;
+        let usuarioElemento = document.createElement('article');
+        usuarioElemento.className = "datos";
+        usuarioElemento.innerHTML = usuarioHTML;
+        datos_container.appendChild(usuarioElemento);
+    
+        if(usuario.status === "Enviada"){
+            usuarioElemento.classList.add("enviada")
+        }else if(usuario.status === "Pendiente") {
+            usuarioElemento.classList.add("pendiente")
+        };
 
-    if(newUser.status === "Enviada"){
-        usuario.classList.add("enviada")
-    }else if(newUser.status === "Pendiente") {
-        usuario.classList.add("pendiente")
-    }
-    if(newUser.status === "Enviada"){
-        usuario.classList.add("enviada")
-    }else if(newUser.status === "Pendiente") {
-        usuario.classList.add("pendiente")
-    }
-};
+        const botonDato = document.getElementById(`botonDato-${usuario.nombre}`);
+        const parrafoStatus = document.getElementById(`parrafoStatus-${usuario.nombre}`);
 
-const botonDato = document.getElementById(`botonDato-${newUser.nombre}`);
-const parrafoStatus = document.getElementById(`parrafoStatus-${newUser.nombre}`);
+        botonDato.addEventListener('click', () => {
+            usuarioElemento.classList.toggle("enviada");
 
-botonDato.addEventListener('click', () => {
-    usuario.classList.toggle("enviada");
-
-    if (usuario.classList.contains("enviada")) {
-        parrafoStatus.textContent = "Enviada";
-    } else {
-        parrafoStatus.textContent = "Pendiente";
-        usuario.classList.add("pendiente");
-    }
-});    
+            if (usuarioElemento.classList.contains("enviada")) {
+                parrafoStatus.textContent = "Enviada";
+            } else {
+                parrafoStatus.textContent = "Pendiente";
+                usuarioElemento.classList.add("pendiente");
+            }
+        });
+    })
+}
+renderizarUsuario(user_list)
